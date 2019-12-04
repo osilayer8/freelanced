dd3bda0469e2c1274ce75ee732a92e9e8323f640
osilayer8 <r_ob@hotmail.de>
2019-12-04 03:30:40 +0100

    allow cross origin

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/users", usersRoutes);
 
 // give back custom error msg if route doesn't exist
