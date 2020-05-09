401ad758e3d3aa5830f7de1919e403c0ff6c46c8
osilayer8 <r_ob@hotmail.de>
2020-05-09 21:49:05 +0200

    test commit

---
+++ ts-frontend/src/shared/components/UIElements/Row.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Row.scss';
+
+const Row: React.FC<{ style?: React.CSSProperties, className?: string }> = (props) => {
+  return (
+    <div className={`row ${props.className ? props.className : ''}`} style={props.style}>
+      {props.children}
+    </div>
+  );
+};
+
+export default Row;
