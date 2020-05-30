079c0d938d83d3bde61fcfa62b6a1d460534ac8b
osilayer8 <r_ob@hotmail.de>
2020-05-31 01:04:45 +0200

    change listing port if prod

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -48,8 +48,7 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
   )
   .then(() => {
-    console.log("Connected to database!");
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
